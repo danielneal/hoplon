a6398a7d5ebcbd7eb080b8f4c9017a083357fb99
Daniel Neal <danielneal2@gmail.com>
2015-01-12 10:00:16 +0000

    Add reveal.js to hoplon

---
+++ contrib/reveal/project.clj
@@ -0,0 +1,8 @@
+(defproject io.hoplon/reveal "0.1.0"
+
+  :description  "Hoplon wrapper for reveal"
+  :license      {:name "Eclipse Public License"
+                 :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [tailrecursion/hoplon "5.10.4"]
+                 [io.hoplon.vendor/reveal "2.6.1-0"]])
---
+++ vendor/reveal/project.clj
@@ -0,0 +1,6 @@
+(defproject io.hoplon.vendor/reveal "2.6.1-0"
+  :description  "Reveal js for presentations"
+  :url          "http://getbootstrap.com"
+  :license      {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
+
