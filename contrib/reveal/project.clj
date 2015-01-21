d37f9f1fa3b828753379a7dfa534c37fa209044a
Daniel Neal <danielneal2@gmail.com>
2015-01-21 10:49:22 +0000

    Modify dependency and group name for reveal

--- contrib/reveal/project.clj
+++ contrib/reveal/project.clj
@@ -1,8 +1,7 @@
-(defproject org.clojars.danielneal/reveal "0.1.0"
-
+(defproject io.hoplon.danielneal/reveal "0.1.0"
   :description  "Hoplon wrapper for reveal"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [tailrecursion/hoplon "5.10.4"]
-                 [io.hoplon.vendor/reveal "2.6.1-0"]])
+                 [io.hoplon.danielneal.vendor/reveal "2.6.1-0"]])
