416e8ccb4ffa0a3949c66583095a78f465448016
Daniel Neal <danielneal2@gmail.com>
2015-02-15 11:48:26 +0000

    Rename namespace to .danielneal so can upload to clojars

--- contrib/dimple/project.clj
+++ contrib/dimple/project.clj
@@ -1,8 +1,8 @@
-(defproject io.hoplon/dimple "0.1.0"
+(defproject io.hoplon.danielneal/dimple "0.1.0"
   :description  "Hoplon wrapper for the dimple charting library."
   :url          "http://github.com/tailrecursion/hoplon/tree/master/contrib/dimple"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [tailrecursion/hoplon "5.10.3"]
-                 [io.hoplon.vendor/dimple "2.1.0-0"]])
+                 [io.hoplon.danielneal.vendor/dimple "2.1.0-0"]])
--- vendor/dimple/project.clj
+++ vendor/dimple/project.clj
@@ -1,7 +1,6 @@
-(defproject io.hoplon.vendor/dimple "2.1.0-0"
+(defproject io.hoplon.danielneal.vendor/dimple "2.1.0-0"
   :description  "D3.js is a JavaScript library for manipulating documents based on data."
   :url          "http://d3js.org"
   :license      {:name "BSD-3-Clause" :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.hoplon.vendor/d3 "3.4.13-0"]])
-
+                 [io.hoplon.danielneal.vendor/d3 "3.4.13-0"]])
