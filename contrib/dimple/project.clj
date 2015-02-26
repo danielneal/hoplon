fe099595de8e8bd18cc713d0af1a8bb5147a1940
Daniel Neal <daniel.neal@evoenergy.co.uk>
2015-02-26 19:38:38 +0000

    Give dimple a simpler API - don't reinvent the wheel for custizing charts

--- contrib/dimple/project.clj
+++ contrib/dimple/project.clj
@@ -1,8 +1,8 @@
-(defproject io.hoplon.danielneal/dimple "0.1.0"
+(defproject io.hoplon.danielneal/dimple "0.2.0"
   :description  "Hoplon wrapper for the dimple charting library."
   :url          "http://github.com/tailrecursion/hoplon/tree/master/contrib/dimple"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [tailrecursion/hoplon "5.10.3"]
+                 [tailrecursion/hoplon "5.10.4"]
                  [io.hoplon.danielneal.vendor/dimple "2.1.0-0"]])
